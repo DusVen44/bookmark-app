57687246daa42a9f789fe204cf65861004c14d07
Dustin Venable <Venny44@yahoo.com>
2020-08-13 16:00:35 -0500

    added edit bookmark

--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
 import BookmarksContext from './../BookmarksContext/BookmarksContext'
 import './BookmarkList.css'
+import PropTypes from 'prop-types'
 
 class BookmarkList extends Component {
   static contextType = BookmarksContext;
@@ -25,3 +26,12 @@ class BookmarkList extends Component {
 }
 
 export default BookmarkList;
+
+BookmarkList.propTypes = {
+  bookmarks: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+    description: PropTypes.string
+  }))
+};
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$cjbWpIigaGJBdS1LJfFZfuadPZn4wOS3e1xOQkXOxtuKbqU2nMTf2',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
